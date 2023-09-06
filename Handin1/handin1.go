97d9e052de13c24238d2f63bb46a1f93a8384814
Clara Sønderborg <auso@itu.dk>
2023-09-06 11:53:13 +0200

    handin1 start

--- Handin1/handin1.go
+++ Handin1/handin1.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import(
+	"fmt"
+	"time"
+) 
+
+var channels = make([]chan bool, 10, 10)
+
+func freud(index int){
+	var hunger int = 3
+
+	for hunger > 0 {
+		think(index)
+		
+	}
+	
+	eat(index, &hunger)
+
+}
+
+func fork(index int){
+
+}
+
+func eat(index int, hunger *int){
+	
+	*hunger = *hunger - 1
+	fmt.Println("Freud number", index, "is eating")
+}
+
+func think(index int){
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Println("Freud number", index, "is thinking")
+}
 
 func main(){
 	fmt.Println("Hello")
+
+	for i := 0; i < 5; i++ {
+		go fork(i)
+	}
+
+	for i := 0; i < 5; i++ {
+		go freud(i)
+	}
 }
