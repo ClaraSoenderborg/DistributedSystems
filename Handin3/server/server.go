b0c95de4ee5ebd4fd7ae65fb0b69d6a51c67438a
Clara Sønderborg <auso@itu.dk>
2023-10-29 13:14:59 +0100

    groupchat virker

--- Handin3/server/server.go
+++ Handin3/server/server.go
@@ -23,28 +23,10 @@ var port = flag.Int("port", 0, "server port number")
 func (s *Server) Broadcast(msgStream proto.ChittyChat_BroadcastServer) error {
     
     s.streams = append(s.streams, msgStream)
-    go s.logOn(msgStream)
+    
+    s.logOn(msgStream)
     go s.receive(msgStream)
-    // get all the messages from the stream
-    // for {
-    //     msg, err := msgStream.Recv()
-    //     if err == io.EOF {
-    //         break
-    //     }
-    //     if err != nil {
-    //         return err
-    //     }
-        // if err := msgStream.Send(msg); err != nil {
-        //     return err
-        // }
-        
-        
-	// every time we get a message from a client, we should immediately broadcast it to all clients (goroutine)
-
-	// how does server know a client has joined? 
-
-    //ack := proto.ServiceBroadcast// make an instance of your return type
-    //msgStream.SendAndClose(ack)
+    
     for {
         
     }
