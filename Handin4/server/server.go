06bfe7da9daaa1a48b0bdcb09f6e07b6661852c4
Julia <jukl@itu.dk>
2023-11-13 15:59:55 +0100

    ha

--- Handin4/server/server.go
+++ Handin4/server/server.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"log"
+	"math/rand"
 	proto "mutex/grpc"
 	"net"
+	"os"
 	"strconv"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 type Node struct {
 	proto.UnimplementedMutexServer // Necessary
-	name                             string
-	port                             int
-	ports []proto.MutexClient
+	id                              int
+	port                            int
+	ports 							[]proto.MutexClient
+	state							string
+	timestamp						int
+	queue							chan int
 }
 
 var port = flag.Int("port", 0, "server port number")
+var id = flag.Int("id", 0, "node id")
 
 func main() {
 	// Get the port from the command line when the server is run
@@ -26,7 +34,7 @@ func main() {
 
 	// Create a server struct
 	node := &Node{
-		name: "serverName",
+		id: *id,
 		port: *port,
 	}
 
@@ -58,16 +66,39 @@ func startNode(n *Node) {
 		log.Fatalf("Could not serve listener")
 	}
 
+	n.state = "RELEASED"
+	n.timestamp = 0
+	n.queue = make(chan int)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	for scanner.Scan(){
+		input := scanner.Text()
+		if (input == "start"){
+			n.connectToPeer()
+
+			y:=rand.Intn(10)
+			if(y>2){
+				log.Printf("Random number: %d",y)
+				n.enter()
+			}
+		}
+	}
+
+
+	
+	
+
 }
 
 func (n *Node)connectToPeer() error {
-
+	log.Printf("Hey girls, i want to connect")
 	for i := 5000; i <= 5005; i++{
 		if(*port != i){
 			// Dial the server at the specified port.
 			conn, err := grpc.Dial("localhost:"+strconv.Itoa(i), grpc.WithTransportCredentials(insecure.NewCredentials()))
 			if err != nil {
-				log.Fatalf("Could not connect to port %d", i)
+				log.Printf("Could not connect to port %d", i)
 			} else {
 				log.Printf("Connected to the server at port %d\n", i)
 				n.ports = append(n.ports, proto.NewMutexClient(conn))
@@ -78,6 +109,42 @@ func (n *Node)connectToPeer() error {
 	return nil
 } 
 
+//This is the recieving function
 func (n *Node) mutex(ctx context.Context, request proto.Request)(*proto.Reply, error){
-	
+	n.timestamp =+1
+
+	if (n.state == "HELD" || (n.state == "WANTED" && (n.timestamp < int(request.Timestamp)))){
+		<- n.queue
+
+	} 
+	return &proto.Reply{
+		Timestamp: int64(n.timestamp),
+		ClientID: int64(n.id),
+	}, nil
 }
+
+func (n *Node) enter(){
+	waittime := time.Duration(rand.Intn(2000))*time.Millisecond
+	time.Sleep(waittime)
+	n.state = "WANTED"
+	n.timestamp =+1
+
+	for _, v := range n.ports{
+		v.Mutex(context.Background(), &proto.Request{
+			Timestamp: int64(n.timestamp),
+			ClientID: int64(n.id),
+		})
+	}
+
+	n.state = "HELD"
+	n.exit()
+}
+
+func (n *Node) exit(){
+	log.Printf("I am node nr.%d and i am doing a critical thing", n.id)
+	n.state = "RELEASED"
+	n.timestamp =+1
+
+	n.queue <- 1
+}
+
